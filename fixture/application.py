0dacde49de35b6e0994ac636fcb0b8b61f1c3bcf
and-ray <redandray@yandex.ru>
2024-09-10 23:31:30 +0300

    добавлены тесты изменения 1х групп и контактов, удаления 1ого контакта.

--- fixture/application.py
+++ fixture/application.py
@@ -8,23 +8,18 @@ class Application:
 
     def __init__(self):
         self.wd = webdriver.Firefox()
-        self.wd.implicitly_wait(30)
+        self.wd.implicitly_wait(60)
         self.session  = SessionHelper(self)
         self.group = GroupHelper(self)
         self.contact  = ContactHelper(self)
-
-
 
     def open_home_page(self):
         wd = self.wd
         wd.get("https://localhost/addressbook/")
 
-
-
     def return_to_home_page(self):
         wd = self.wd
         wd.find_element_by_link_text("home").click()
-
 
     def destroy(self):
         self.wd.quit()
--- fixture/contact.py
+++ fixture/contact.py
@@ -1,3 +1,6 @@
+from selenium.webdriver.common.by import By
+
+
 class ContactHelper:
     def __init__(self, app):
         self.app = app
@@ -5,6 +8,39 @@ class ContactHelper:
     def create(self, contact):
         wd = self.app.wd
         wd.find_element_by_link_text("add new").click()
+        wd.find_element_by_name("firstname").click()
+        wd.find_element_by_name("firstname").clear()
+        wd.find_element_by_name("firstname").send_keys(contact.first_name)
+        wd.find_element_by_name("middlename").click()
+        wd.find_element_by_name("middlename").clear()
+        wd.find_element_by_name("middlename").send_keys(contact.middle_name)
+        wd.find_element_by_name("lastname").click()
+        wd.find_element_by_name("lastname").clear()
+        wd.find_element_by_name("lastname").send_keys(contact.last_name)
+        wd.find_element_by_name("home").click()
+        wd.find_element_by_name("home").clear()
+        wd.find_element_by_name("home").send_keys(contact.home_phone)
+        wd.find_element_by_name("email").click()
+        wd.find_element_by_name("email").clear()
+        wd.find_element_by_name("email").send_keys(contact.email)
+        wd.find_element_by_xpath("//div[@id='content']/form/input[20]").click()
+        self.app.return_to_home_page()
+
+    def delete_first_contact(self):
+        wd = self.app.wd
+        self.app.return_to_home_page()
+        # select
+        wd.find_element_by_name("selected[]").click()
+        # exterminate
+        wd.find_element(By.XPATH, "//input[@value='Delete']").click()
+        self.app.return_to_home_page()
+
+    def modify_first(self, contact):
+        wd = self.app.wd
+        self.app.return_to_home_page()
+        # select
+        wd.find_element(By.XPATH, "//img[@title='Edit']").click()
+        #edit
         wd.find_element_by_name("firstname").click()
         wd.find_element_by_name("firstname").clear()
         wd.find_element_by_name("firstname").send_keys(contact.first_name)
