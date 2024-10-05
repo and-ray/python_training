d4b991f2117767b2466f9f7e2f96854ef74a396d
and-ray <redandray@yandex.ru>
2024-10-05 21:16:13 +0300

    некорректно сравнивает контакты

--- model/contact.py
+++ model/contact.py
@@ -1,22 +1,39 @@
 from sys import maxsize
 
 class Contact:
-    def __init__(self, first_name=None, middle_name=None, last_name=None, home_phone=None, email=None, id=None):
+    def __init__(self, first_name=None, middle_name=None, last_name=None, id=None, address=None,
+                 email1=None,  email2=None,  email3=None,
+                 home_phone=None,mobile_phone = None, work_phone = None, all_phones_from_home_page=None,
+                 all_emails_from_home_page=None): #, secondary_phone = None
         self.first_name = first_name
         self.middle_name = middle_name
         self.last_name = last_name
         self.home_phone = home_phone
-        self.email = email
+        self.mobile_phone = mobile_phone
+        self.work_phone = work_phone
+        self.all_phones_from_home_page = all_phones_from_home_page
+        self.all_emails_from_home_page = all_emails_from_home_page
+        self.email1 = email1
+        self.email2 = email2
+        self.email3 = email3
+        self.address = address
         self.id = id
 
     def __repr__(self):
-        return "Объект включает в себя: %s, %s, %s" % (self.id, self.first_name,  self.last_name) #self.middle_name, , self.home_phone, self.email
+        return ("id=%s, first_name=%s, middle_name=%s, last_name=%s,  home_phone=%s, mobile_phone=%s, work_phone=%s, "
+                "email1=%s, email2=%s, email3=%s, address=%s"
+               #, "all_phones_from_home_page%s, all_emails_from_home_page%s"
+                ) %(self.id, self.first_name,  self.middle_name, self.last_name,
+                                                                              self.home_phone, self.mobile_phone, self.work_phone,
+                                                                              self.email1, self.email2, self.email3, self.address,
+                                                                           #   self.all_phones_from_home_page, self.all_emails_from_home_page
+                    )
 
     def __eq__(self, other):
-        return self.first_name == other.first_name and (self.id is None or other.id is None or self.id == other.id)
+        return (self.id is None or other.id is None or self.id == other.id) and self.first_name == other.first_name and self.last_name == other.last_name
 
-    def id_or_max(cont):
-        if cont.id:
-            return int(cont.id)
+    def id_or_max(self):
+        if self.id:
+            return int(self.id)
         else:
             return maxsize
