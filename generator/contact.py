935f00e2752451c4e97554c21b504fc154e10580
and-ray <redandray@yandex.ru>
2024-10-06 18:37:10 +0300

    генератор json файлов для контактов

---
+++ generator/contact.py
@@ -0,0 +1,56 @@
+from model.contact import Contact
+import random
+import string
+import os.path
+import jsonpickle
+import getopt
+import sys
+
+
+try:
+    opts, args = getopt.getopt(sys.argv[1:], "n:f:", ["number of contacts","file"])
+except getopt.GetoptError as err:
+    getopt.usage()
+    sys.exit(2)
+
+n = 5
+f = "data/contacts.json"
+
+for o, a in opts:
+    if o == "-n":
+        n = int(a)
+    elif o =="-f":
+        f  = a
+
+file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "../", f)
+
+def random_string (prefix, maxlen):
+    symbols = string.ascii_letters + " " + string.digits
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+def random_digits (prefix, maxlen):
+    symbols = string.digits + " "*10
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+def random_email (prefix, maxlen):
+    symbols = string.ascii_letters + string.digits
+    domain = string.ascii_letters + string.digits
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])+"@"+"".join([random.choice(domain) for i in range(random.randrange(maxlen))])+".ru"
+
+
+testdata = [
+Contact(first_name=first_name, middle_name=middle_name, last_name=last_name,
+        home_phone=home_phone,work_phone=work_phone, mobile_phone=mobile_phone, email1=email1)
+    for first_name in ["", random_string("first_name", 10)]
+    for middle_name in [random_string("middle_name", 20)]
+    for last_name in ["", random_string("last_name", 20)]
+    for home_phone in [random_digits("home_phone", 20)]
+    for work_phone in [random_digits("work_phone", 20)]
+    for mobile_phone in [random_digits("mobile_phone", 20)]
+    for email1 in [random_email("email1", 10)]
+]
+
+
+with open(file, "w") as out:
+    jsonpickle.set_encoder_options("json", indent=2)
+    out.write(jsonpickle.encode(testdata))
--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,36 +1,9 @@
 # -*- coding: utf-8 -*-
 from model.contact import Contact
 import pytest
-import random
-import string
+from data.contacts import testdata
 
-def random_string (prefix, maxlen):
-    symbols = string.ascii_letters + " " + string.digits
-    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
-
-def random_digits (prefix, maxlen):
-    symbols = string.digits + " "*10
-    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
-
-def random_email (prefix):
-    symbols = string.ascii_letters + string.digits
-    domain = string.ascii_letters + string.digits
-    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(10))])+"@"+"".join([random.choice(domain) for i in range(random.randrange(10))])+".ru"
-
-
-testData = [
-Contact(first_name=first_name, middle_name=middle_name, last_name=last_name,
-        home_phone=home_phone,work_phone=work_phone, mobile_phone=mobile_phone, email1=email1)
-    for first_name in ["", random_string("first_name", 10)]
-    for middle_name in [random_string("middle_name", 20)]
-    for last_name in ["", random_string("last_name", 20)]
-    for home_phone in [random_digits("home_phone", 20)]
-    for work_phone in [random_digits("work_phone", 20)]
-    for mobile_phone in [random_digits("mobile_phone", 20)]
-    for email1 in [random_string("email1", 20)]
-]
-
-@pytest.mark.parametrize("new_contact", testData, ids=[repr(x) for x in testData])
+@pytest.mark.parametrize("new_contact", testdata, ids=[repr(x) for x in testdata])
 def test_add_contact(app, new_contact):
     old_contact_list = app.contact.get_contact_list()
     app.contact.create(new_contact)
