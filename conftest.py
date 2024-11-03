c28a2da349d7e0e1a8e1726d159c416044aa2fd7
and-ray <redandray@yandex.ru>
2024-11-04 00:49:55 +0300

    python 3.9 jsons

--- conftest.py
+++ conftest.py
@@ -70,6 +70,6 @@ def load_from_module(module):
     return importlib.import_module("data.%s" % module).testdata
 
 def load_from_json(file):
-    with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), "data/%s.json" % file)) as f:
+    with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), "data\%s.json" % file)) as f:
         return jsonpickle.decode(f.read())
 
